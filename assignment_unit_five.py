55b63f614af37c6046556d1f30b27415a5a68948
Frank <frank@iMac3.local>
2018-10-16 12:07:44 -0400

    I know what I need to work on next class.

--- assignment_unit_five.py
+++ assignment_unit_five.py
@@ -1,6 +1,20 @@
 import random
 
 
-random.ramdint(1, 100)
+def number():
+    number = random.ramdint(1, 100)
+    return number
 
 
+while True:
+    answer = int(input("Would you like to guess my number?"))
+    if answer == number:
+        print("congratulation!")
+        break
+    else:
+        if answer 
+
+
+def main():
+    number()
+    answer()
