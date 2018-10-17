a75bf15980dd521d95d65b09f1df4053072f5132
Frank <frank@iMac3.local>
2018-10-17 13:49:32 -0400

    I almost finished

--- assignment_unit_five.py
+++ assignment_unit_five.py
@@ -2,19 +2,30 @@ import random
 
 
 def number():
-    number = random.ramdint(1, 100)
-    return number
-
-
-while True:
-    answer = int(input("Would you like to guess my number?"))
-    if answer == number:
-        print("congratulation!")
-        break
-    else:
-        if answer 
+    correct_answer = random.randint(1, 100)
+    return correct_answer
 
 
 def main():
-    number()
-    answer()
+    while True:
+        answer1 = input("Do you want to start a game? put in 'y' or 'n'")
+        if answer1 is "n":
+            print("ok")
+            break
+        elif answer1 is "y":
+            print("let's start")
+            answer2 = number()
+            while True:
+                users_guess = int(input("Give a number you want to guess."))
+                if users_guess == answer2:
+                    print("Congratulation!")
+                    break
+                elif users_guess > answer2:
+                    print("your guess was too high")
+                else:
+                    print("Your guess was too low")
+        else:
+            print("your answer is not valid")
+
+
+main()
